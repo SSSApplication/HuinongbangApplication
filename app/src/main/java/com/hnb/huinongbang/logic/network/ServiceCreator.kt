8118aa0a2b0ae072e22af17d7812fbfa0cb5d151
chengxianjie <1251996987@qq.com>
2021-01-30 15:29:13 +0800

    改地址

--- app/src/main/java/com/hnb/huinongbang/logic/network/ServiceCreator.kt
+++ app/src/main/java/com/hnb/huinongbang/logic/network/ServiceCreator.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceCreator {
     //    模拟器 "http://10.0.2.2/"
     // 域名 "https://www.huinongbang.club/"
-    private const val BASE_URL = "http://10.0.2.2/"
+    private const val BASE_URL = "https://www.huinongbang.club/"
     val lunbo = BASE_URL+"img/lunbo/"
     val lunbo_donate = BASE_URL+"webphoto/"
     private val retrofit = Retrofit.Builder()
