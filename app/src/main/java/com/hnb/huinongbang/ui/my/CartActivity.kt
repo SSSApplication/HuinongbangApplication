4c2516a9da6e5c71c93785dedd68c0626b31a58d
彭浩然 <347660650@qq.com>
2021-03-24 22:18:27 +0800

    个人中心购物车和计划清单（没有页面样式）

---
+++ app/src/main/java/com/hnb/huinongbang/ui/my/CartActivity.kt
@@ -0,0 +1,42 @@
+package com.hnb.huinongbang.ui.my
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import com.hnb.huinongbang.HNBApplication
+import com.hnb.huinongbang.R
+import com.hnb.huinongbang.logic.Repository
+import com.hnb.huinongbang.logic.model.GetCartData
+import com.hnb.huinongbang.logic.model.IdentityData
+import com.hnb.huinongbang.util.ToastUtil
+import kotlinx.android.synthetic.main.activity_identity.*
+
+class CartActivity : AppCompatActivity() {
+
+    val viewModel by lazy { ViewModelProviders.of(this).get(CartViewModel::class.java)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+
+        //获取用户类
+        val user = Repository.getUser()
+
+        viewModel.getCart(
+            GetCartData(
+                user.user_ID.toString(),
+                0.toString()
+            )
+        )
+        //监听提交结果
+        viewModel.getCartResult.observe(this, { result ->
+            val list = result.getOrNull()
+            if (list != null) {
+                ToastUtil.show(list.toString())
+            } else {
+                ToastUtil.show("获取失败")
+            }
+        })
+    }
+}
